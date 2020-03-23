a8757964ab59a617d6b8e9abca6f170f1208306d
Kacper <kac.motyka@gmail.com>
2020-03-23 03:46:15 +0100

    add strategy table to view model and controler

--- IA-java/src/com/company/GUI_FX/Controller.java
+++ IA-java/src/com/company/GUI_FX/Controller.java
@@ -1,11 +1,22 @@
 package com.company.GUI_FX;
 
+import com.company.GUI_swing.StrategyTableComponent;
+import com.company.LOGIC.DataManagerPseudo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ToggleGroup;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.embed.swing.SwingNode;
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 //import java.awt.ActiveEvent;
 
-public class Controller {
+public class Controller implements Initializable {
 
         @FXML
         private ToggleGroup Action1;
@@ -13,27 +24,64 @@ public class Controller {
         @FXML
         private ToggleGroup Action2;
 
-        private String currentGame;
         @FXML
+        private Node currentLogo;
+        @FXML
+        private Node currentGameLabel;
 
+        @FXML
+        private Node currentStrategyLabel;
+
+        @FXML
+        private Node currentResultsPane;
+
+        @FXML
+        private Node scrollPane;
+
+        @FXML
         void chooseGameRadio(ActionEvent event) {
-
-        if (event.getSource().equals("Lotto")){
-            currentGame = "lotto";
-
-            }
         }
 
         @FXML
         void chooseResultsRadio(ActionEvent event) {
-
         }
 
         @FXML
         void refreshDataButton(ActionEvent event) {
+        }
 
+        @FXML
+        private SwingNode currentTable;
+
+        @Override
+        public void initialize(URL location, ResourceBundle resources) {
+                //SwingNode currentTable = new SwingNode();
+                //Pane pane = new Pane();
+                createAndSetSwingCurrentTable(currentTable, new DataManagerPseudo());
+                //pane.getChildren().add(currentTable);
+        }
+
+        public void createAndSetSwingCurrentTable(final SwingNode swingNode, DataManagerPseudo dataManager) {
+                SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                                swingNode.setContent(new StrategyTableComponent(new Integer[]{1,12,3,34,5,46}));
+                        }
+                });
+        }
+        private void createSwingContent(final SwingNode swingNode, DataManagerPseudo dataManager) {
+                SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                                //swingNode.setContent(new JButton("Click me!"));
+                                //swingNode.setContent(new HTMLButton());
+                                swingNode.setContent(new StrategyTableComponent(new Integer[]{1,12,3,34,5,46}));
+                        }
+
+                });
         }
 }
+
 
 
 
--- IA-java/src/com/company/GUI_FX/Model.java
+++ IA-java/src/com/company/GUI_FX/Model.java
@@ -1,36 +1,99 @@
 package com.company.GUI_FX;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.Parent;
+
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+
+
+
+
+import javafx.embed.swing.SwingNode;
+import javax.swing.SwingUtilities;
+import javax.swing.JButton;
+import javafx.scene.Node;
+import javafx.scene.control.ToggleGroup;
+
+import com.company.LOGIC.DataManagerPseudo;
 import com.company.LOGIC.DataManager;
 import com.company.LOGIC.Game;
+import com.company.GUI_swing.*;
 
 public class Model extends javafx.application.Application {
 
-    DataManager data;
+    DataManagerPseudo dataManager;
     Game currentGame;
 
+
+    private void createSwingContent(final SwingNode swingNode, DataManagerPseudo dataManager) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //swingNode.setContent(new JButton("Click me!"));
+                //swingNode.setContent(new HTMLButton());
+                swingNode.setContent(new StrategyTableComponent(new Integer[]{1,12,3,34,5,46}));
+            }
+
+        });
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println("Inside init() method! Perform necessary initializations here.");
+        //DataManagerPseudo dataManager = new  DataManagerPseudo();
+        //Game currentGame = dataManager.lotto;
+    }
+    /*
     private Model(DataManager dataManager){
         this.data = dataManager;
         this.currentGame = data.lotto;
     }
-
+*/
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Million + Swing in JavaFX");
+
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-        primaryStage.setTitle("Calculator");
-        // primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setScene(new Scene(root, 1000, 800));
-        //primaryStage.setResizable(true);
-        primaryStage.show();
+        //stage.setScene(new Scene(root, 1000, 800));
+
+        //SwingNode currentTable = new SwingNode();
+        //createSwingContent(currentTable, new DataManagerPseudo());
+        StackPane pane = new StackPane();
+
+        pane.getChildren().add(root);
+        //pane.getChildren().add(currentTable);
+        //pane.getChildren().addAll(root, currentTable);
+
+        // System.out.println(pane.getChildren().get(0).toString());
+        // pane.getChildren().add(swingNode);
+        // Node resultpane = (Node) pane.lookup("#scrollPane");
+
+        stage.setScene(new Scene(pane, 1000, 800));
+
+        stage.setResizable(true);
+        stage.show();
+
     }
 
 
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Inside stop() method! Destroy resources. Perform Cleanup.");
+        // save results
+    }
+
     public static void main(String[] args) {
+
+        //DataManagerPseudo dataManager = new  DataManagerPseudo();
+        //Model model = new Model(dataManager);
         launch(args);
-        DataManager data = new DataManager();
-        Model gui = new Model(data);
     }
 }
